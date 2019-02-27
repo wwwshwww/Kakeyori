76d69eca80d74dc5733c47d8e20fb7e7272d0399
Reona Sato <www.shinderu.www@gmail.com>
2019-02-27 18:57:03 +0900

    fix to use module of calibration

--- streamCalibrated.py
+++ streamCalibrated.py
@@ -1,14 +1,7 @@
-import numpy as np
 import cv2
-import pickle
-import pprint
+from calibration import calibrated
 
-try:
-    with open('calibration/camera_matrix', 'rb') as file:
-        cam_mtx = pickle.load(file)
-        pprint.pprint(cam_mtx)
-except FileNotFoundError:
-    print('Camera matrix not fonud. Please run `calibration/realtimeCalibrate.py` before.')
+cam_mtx = calibrated.getCameraMatrix()
 
 cap = cv2.VideoCapture(1)
 count = 0
@@ -16,15 +9,8 @@ count = 0
 while True:
     ret, frame = cap.read()
     # frame = cv2.resize(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)))
-    h, w = frame.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cam_mtx['mtx'], cam_mtx['dist'], (w,h), 0, (w,h))
-
-    mapx, mapy = cv2.initUndistortRectifyMap(cam_mtx['mtx'], cam_mtx['dist'], None, newcameramtx, (w,h), 5)
-    dst = cv2.remap(frame, mapx, mapy, cv2.INTER_LINEAR)
-
-    x, y, w, h = roi
-    dst = dst[y:y+h, x:x+w]
-    cv2.imshow('frame', dst)
+    result = calibrated.calibration(frame, cam_mtx)
+    cv2.imshow('frame', result)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
