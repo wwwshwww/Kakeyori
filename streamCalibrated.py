db17878f732a75754f883afe0ed8110b1e193c4e
Reona Sato <www.shinderu.www@gmail.com>
2019-03-06 18:46:22 +0900

    fix stream

--- streamCalibrated.py
+++ streamCalibrated.py
@@ -4,13 +4,12 @@ from calibration import calibrated
 cam_mtx = calibrated.getCameraMatrix()
 
 cap = cv2.VideoCapture(1)
-count = 0
 
 while True:
     ret, frame = cap.read()
     # frame = cv2.resize(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)))
-    result = calibrated.calibration(frame, cam_mtx)
-    cv2.imshow('frame', result)
+    result = calibrated.calibration(frame, cam_mtx, 1)
+    cv2.imshow('result', result)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
