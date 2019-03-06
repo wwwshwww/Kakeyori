f54dc90ff0145c80f5b54682cf2485d539fa2b2b
Reona Sato <www.shinderu.www@gmail.com>
2019-03-06 18:47:33 +0900

    tweak function in single calibration

--- calibration/realtimeCalibrate.py
+++ calibration/realtimeCalibrate.py
@@ -9,6 +9,8 @@ import pprint
 args = sys.argv
 cap = cv2.VideoCapture(1)
 
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 if len(args) == 3:
     cols = int(args[1])
     rows = int(args[2])
@@ -25,33 +27,37 @@ imgpoints = [] # 2d points in image plane
 result_matrix = {}
 
 count = 0
-COUNT_MAX = 3
+COUNT_MAX = 40
 
 while True:
     re, frame = cap.read()
 
     # frame = cv2.resize(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)))
-    flg = cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FILTER_QUADS + cv2.CALIB_CB_FAST_CHECK
-    ret, corners = cv2.findChessboardCorners(gray, (cols, rows), flags=flg)
-    # ret, corners = cv2.findCirclesGrid(gray, (cols, rows), flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    flg = cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_FILTER_QUADS + cv2.CALIB_CB_FAST_CHECK
+    ret, corners = cv2.findChessboardCorners(frame, (cols, rows), flags=flg)
+    # ret, corners = cv2.findCirclesGrid(frame, (cols, rows), flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
 
     # success find chessboard
     if ret:
         objpoints.append(objp)
 
+        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+
         imgpoints.append(corners)
 
-        frame = cv2.drawChessboardCorners(frame, (cols, rows), corners, ret)
+        cv2.drawChessboardCorners(frame, (cols, rows), corners, ret)
         print(corners)
         print(count)
 
         count += 1
 
     cv2.imshow('image', frame)
-    sleep(0.4)
+    sleep(0.3)
 
     if count > COUNT_MAX:
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+        flag = 0
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None,flags=flag)
         result_matrix['ret'] = ret
         result_matrix['mtx'] = mtx
         result_matrix['dist'] = dist
