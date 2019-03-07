e23d1427b30b74f64c1a97f0a83413c430f834d8
Reona Sato <www.shinderu.www@gmail.com>
2019-03-07 22:50:23 +0900

    add program of realtime calibration for fisheye model

---
+++ calibration/realtimeCalibrate_wide.py
@@ -0,0 +1,60 @@
+import cv2
+# import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from time import sleep
+import pickle
+import pprint
+
+from cameraModel import WideCamera
+
+args = sys.argv
+
+COLS = 6
+ROWS = 7
+
+if len(args) == 2:
+    COUNT_MAX = int(args[1])
+else:
+    COUNT_MAX = 40
+
+count = 0
+
+result = {}
+cam1 = WideCamera(1, (COLS,ROWS))
+
+while True:
+    cam1.read()
+    frame = cam1.getFrame()
+    cam1.findChess()
+    # success find chessboard
+    if cam1.isFoundChess():
+        cam1.appendPoints()
+        frame = cam1.drawChess()
+
+        print(cam1.getCorners())
+        print(count)
+        count += 1
+
+    cv2.imshow('image', frame)
+    sleep(0.3)
+
+    if count > COUNT_MAX:
+        ret, K, D, rvecs, tvecs = cam1.calibrate()
+        result['ret'] = ret
+        result['K'] = K
+        result['D'] = D
+        result['rvecs'] = rvecs
+        result['tvecs'] = tvecs
+
+        pprint.pprint(result)
+        with open('calibration/wide_result', 'wb') as output:
+            pickle.dump(result, output)
+
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+del cam1
+cv2.destroyAllWindows()
