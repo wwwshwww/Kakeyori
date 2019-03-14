af57344c5e11497150986012c15ce0711767e094
Reona Sato <www.shinderu.www@gmail.com>
2019-03-14 18:41:19 +0900

    add means to confirm stereo disparity view

--- streamStereoCollimated_wide.py
+++ streamStereoCollimated_wide.py
@@ -16,11 +16,30 @@ while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
 
-    re_frame1 = cv2.remap(frame2, maps['map1_l'], maps['map2_l'], interpolation)
-    re_frame2 = cv2.remap(frame1, maps['map1_r'], maps['map2_r'], interpolation)
+    re_frame1 = cv2.remap(frame1, maps['map1_l'], maps['map2_l'], interpolation)
+    re_frame2 = cv2.remap(frame2, maps['map1_r'], maps['map2_r'], interpolation)
 
     stacked = np.hstack((re_frame1[:,:], re_frame2[:,:]))
     cv2.imshow('collimated views', stacked)
+
+    gray1 = cv2.cvtColor(re_frame1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(re_frame2, cv2.COLOR_BGR2GRAY)
+
+    stereo = cv2.StereoBM_create(80,5)
+    # disparity = stereo.compute(gray1, gray2)
+
+    # block_size = 27
+    # min_disp = 0
+    # num_disp = 112
+    # stereo = cv2.StereoSGBM_create(
+    #     minDisparity = min_disp,           # 視差の下限
+    #     numDisparities = num_disp,        # 最大の上限
+    #     blockSize = block_size,
+    #     mode = True
+    # )
+
+    disparity = stereo.compute(gray1, gray2).astype(np.float32) / 32.0
+    cv2.imshow('disparity', disparity)
 
     # cv2.imshow('collimated view1', re_frame1)
     # cv2.imshow('collimated view2', re_frame2)
