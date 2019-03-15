15f8f5dab51a182b4341328a6242aa8ed951be1d
Reona Sato <www.shinderu.www@gmail.com>
2019-03-15 16:30:17 +0900

    increase accuracy of disparity map

--- streamStereoCollimated_wide.py
+++ streamStereoCollimated_wide.py
@@ -5,7 +5,7 @@ from calibration import calibrated
 cap1 = cv2.VideoCapture(1)
 cap2 = cv2.VideoCapture(2)
 
-interpolation = cv2.INTER_NEAREST
+interpolation = cv2.INTER_CUBIC
 
 rect = calibrated.getStereoRect()
 maps = calibrated.getStereoMap()
@@ -16,8 +16,11 @@ while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
 
-    re_frame1 = cv2.remap(frame1, maps['map1_l'], maps['map2_l'], interpolation)
-    re_frame2 = cv2.remap(frame2, maps['map1_r'], maps['map2_r'], interpolation)
+    re_frame1 = cv2.remap(frame2, maps['map1_l'], maps['map2_l'], interpolation)
+    re_frame2 = cv2.remap(frame1, maps['map1_r'], maps['map2_r'], interpolation)
+
+    # re_frame1 = cv2.blur(re_frame1,(5,5))
+    # re_frame2 = cv2.blur(re_frame2,(5,5))
 
     stacked = np.hstack((re_frame1[:,:], re_frame2[:,:]))
     cv2.imshow('collimated views', stacked)
@@ -25,8 +28,11 @@ while True:
     gray1 = cv2.cvtColor(re_frame1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(re_frame2, cv2.COLOR_BGR2GRAY)
 
+    # 画像のヒストグラム平坦化・平滑化
+    gray1 = cv2.GaussianBlur(cv2.equalizeHist(gray1),(5,5), 0)
+    gray2 = cv2.GaussianBlur(cv2.equalizeHist(gray2),(5,5), 0)
+
     stereo = cv2.StereoBM_create(80,5)
-    # disparity = stereo.compute(gray1, gray2)
 
     # block_size = 27
     # min_disp = 0
@@ -38,11 +44,14 @@ while True:
     #     mode = True
     # )
 
-    disparity = stereo.compute(gray1, gray2).astype(np.float32) / 32.0
-    cv2.imshow('disparity', disparity)
 
-    # cv2.imshow('collimated view1', re_frame1)
-    # cv2.imshow('collimated view2', re_frame2)
+    disparity = stereo.compute(gray1, gray2).astype(np.float32) / 700.0
+    # disparity = cv2.GaussianBlur(disparity,(21,21),0)
+    # disparity = cv2.medianBlur(disparity, 5)
+    for i in range(3):
+        # disparity = cv2.blur(disparity, (1+(i*2),1+(i*2)))
+        disparity = cv2.GaussianBlur(disparity,(7, 7),0)
+    cv2.imshow('disparity', disparity)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
